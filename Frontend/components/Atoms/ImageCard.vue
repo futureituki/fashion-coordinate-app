ab33336332d7233747a720ad440abf4250b7e871
“Itsuki <satoitsuki0417@gmail.com>
2023-05-03 01:59:40 +0900

    コンポーネント追加

--- Frontend/components/Atoms/ImageCard.vue
+++ Frontend/components/Atoms/ImageCard.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="mx-10 my-3">
-    <button class="w-full rounded p-2 text-white" :class="[bgColor]">
-      <slot />
-    </button>
-  </div>
+  <nuxt-link class="card h-auto relative max-w-sm block">
+    <div
+      class="overflow-hidden w-full h-full bg-gray-100 md:absolute left-0 top-0"
+    >
+      <div class="img h-full"></div>
+    </div>
+  </nuxt-link>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  padding-top: 56%;
+  margin-bottom: 6.3%;
+  width: 45%;
+}
+.img {
+  background: url('/test2.jpeg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,20 +1,35 @@
 <template>
-  <section class="mt-8 mb-8">
+  <section class="mt-14 mb-8">
     <div class="mt-4 mb-4 grid place-items-center">
       <span class="leading-4 tracking-wide font-bold"
         >さっそくコーデを見てみよう</span
       >
     </div>
     <Tab :activeItem="activeItem" :tabItems="tabItems" />
+    <div class="mt-14 mb-14">
+      <div class="flex gap-10 justify-center flex-wrap">
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </div>
+    </div>
+    <button @click="modal">開く</button>
+    <ModalWindow :show="show" />
   </section>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import Tab from '~/components/Molecules/TabBar.vue'
+import ImageCard from '~/components/Atoms/ImageCard.vue'
+import ModalWindow from '~/components/Atoms/ModalWindow.vue'
 const activeItem = ref(1)
+const show = ref(false)
 const tabItems = [
   { tabId: 1, tabName: '人気' },
   { tabId: 2, tabName: '新着' },
 ]
+const modal = () => {
+  show.value = !show.value
+}
 </script>
